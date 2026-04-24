d093603c9d7c589dbc487007683bc1bfbc9fccd5
willfreit4s <williansilvarj96@gmail.com>
2026-04-24 11:08:55 -0300

    fix: change the migrations

--- sql/migrations/000001_add_tables_token_and_token_metrics.up.sql
+++ sql/migrations/000001_add_tables_token_and_token_metrics.up.sql
@@ -15,23 +15,24 @@ CREATE TABLE IF NOT EXISTS tokens (
 );
 
 CREATE TABLE IF NOT EXISTS token_metrics (
-    id BIGSERIAL,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     token_id UUID NOT NULL,
     token_address VARCHAR(42) NOT NULL,
-    chain VARCHAR(50) NOT NULL,
     price DOUBLE PRECISION NOT NULL DEFAULT 0,
     price_change_24h DOUBLE PRECISION NOT NULL DEFAULT 0,
-    price_change_7d DOUBLE PRECISION DEFAULT 0,
     volume_24h DOUBLE PRECISION NOT NULL DEFAULT 0,
     market_cap DOUBLE PRECISION NOT NULL DEFAULT 0,
     holders BIGINT NOT NULL DEFAULT 0,
     transactions_24h BIGINT NOT NULL DEFAULT 0,
-    liquidity_usd DOUBLE PRECISION DEFAULT 0,
-    fully_diluted_valuation DOUBLE PRECISION DEFAULT 0,
     timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     deleted_at TIMESTAMPTZ,
     deleted BOOLEAN NOT NULL DEFAULT FALSE
 );
+
+-- Constraints
+ALTER TABLE token_metrics ADD CONSTRAINT fk_token_metrics_token FOREIGN KEY (token_id) REFERENCES tokens(id);
+
+-- Unique
+ALTER TABLE tokens ADD CONSTRAINT uk_tokens_address_chain UNIQUE (address, chain);
 
 COMMIT;
